cb276b343e8f525e6d950aab39506a4feaad85d9
Steve Silva <Steve.Silva@copart.com>
2023-01-19 09:25:39 -0300

    install pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import api from './services/Api';
 
 function App() {
+  const [category, setCategory] = useState();
+
+  useEffect(() =>{
+
+  }, []);
+
+  function saveCategory(): void {
+    api.post("https:127.0.0.1:4200/category/add",{
+      Description: "Tirinha"
+    })
+      .then((resp) => console.log(resp))
+      .then(resp => { console.log(resp); })
+      .catch((err) => console.log(err))
+  }
+
+  function listCategory(): void {
+    api
+    .get('start')
+    .then((response) => console.log(response))
+    .catch((err) => console.log(err))
+  }
+
+
+  function handleChangeCategory(e: any) {
+    setCategory(e.target.value);
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={(e) => handleChangeCategory(e)} />
+      <button onClick={listCategory}>Listar</button>
+      <button onClick={saveCategory}>Salvar</button>
     </div>
   );
 }
