8cc5c5ab8365f468246eef7536f0deed056d5e1a
Steve Silva <Steve.Silva@copart.com>
2023-01-23 18:16:12 -0300

    text api

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import api from './services/Api';
 
 function App() {
+  const [genericList, setGenericList] = useState();
   const [category, setCategory] = useState();
 
-  useEffect(() =>{
-
-  }, []);
-
   function saveCategory(): void {
-    api.post("https:127.0.0.1:4200/category/add",{
-      Description: "Tirinha"
+    api.post('category',{
+      Description: category
     })
       .then((resp) => console.log(resp))
       .then(resp => { console.log(resp); })
       .catch((err) => console.log(err))
   }
 
-  function listCategory(): void {
-    api
-    .get('start')
-    .then((response) => console.log(response))
-    .catch((err) => console.log(err))
+  async function listCategory() {
+    console.log("categoria escolhida "+ category);
+    api.get("/status", {
+      headers:{
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+      }
+    })
+    .then((resp) => {
+      console.log(resp);
+      setGenericList(resp.data);
+
+    })
+    .catch((ex)=>console.log);
   }
 
-
-  function handleChangeCategory(e: any) {
+  function handleChangeCategory(e:any){
     setCategory(e.target.value);
   }
 
   return (
 
     <div className="App">
-      <input type="text" onChange={(e) => handleChangeCategory(e)} />
+      <input type="text"  onChange={handleChangeCategory}/>
       <button onClick={listCategory}>Listar</button>
       <button onClick={saveCategory}>Salvar</button>
+
+      <p>{genericList}</p>
     </div>
   );
 }
--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+
+const serviceURL = process.env.REACT_APP_SERVICE_URL;
+const localURL = "http://localhost:4200";
+
+console.log(serviceURL+ "service");
+
 const api = axios.create({
-  baseURL: "https:localhost:4200/",
-});
+    baseURL: localURL});
 
 export default api;
